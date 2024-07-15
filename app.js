997d9b0fa0f14a109883a7d3551cc657828047eb
KastroF <nkastrro@gmail.com>
2024-07-15 22:52:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express"); 
+const mongoose = require("mongoose");
+
+const cors = require('cors');
+const requestMap = new Map();
+
+
+const app = express();
+app.use(cors());
+
+app.use(express.json({limit: "50mb"})); 
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    next();
+  });
+
+mongoose.connect("mongodb+srv://emoneysarl:2ItTxqHrRla49VUU@cluster0.pan21qj.mongodb.net/emoney?retryWrites=true&w=majority",
+
+  { useNewUrlParser: true,
+    useUnifiedTopology: true, autoIndex: true })
+  .then(() => {
+  
+  
+  console.log('Connexion à MongoDB réussie !'); 
+     
+              
+              }) 
+
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+  app.use("/", (req, res) => {
+        
+    console.log("On y est bien")
+        res.status(201).json({status: 0, message: "Vous êtes à bon port"});
+  })
+
+
+  module.exports = app;
